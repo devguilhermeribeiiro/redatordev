6abff23fc22c0e59327aa76c2b4d4397736be0ae
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-12 17:09:51 -0300

    Configurating home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,16 @@
 class HomeController < ApplicationController
-
+  before_action :set_article, only: %i[ show ]
   def index
     @articles = Article.all
   end
 
   # GET /articles/1 or /articles/1.json
   def show
+  end
+
+  private
+
+  def set_article
+    @articles = Article.find(params[:id])
   end
 end
