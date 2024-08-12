cf1c52bb15bbb10441eade07459603f08b2858b8
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-12 15:54:28 -0300

    Configuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :articles
-  root "articles#index"
+  root "articles#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
