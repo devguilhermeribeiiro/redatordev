5bc0658737e71b7e656f952ec5d1fb3930691e26
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-15 18:26:59 -0300

    Reseting database and run migrations again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_14_012410) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_15_211105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_012410) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "tag"
+    t.bigint "tag_id", null: false
+    t.index ["tag_id"], name: "index_articles_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +43,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_012410) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "tags"
 end
