32354b8408c45665990eff330502fde6dee5f3e0
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-15 19:05:44 -0300

    Altering articles model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ApplicationRecord
   validates_presence_of :title, presence: true
   validates_presence_of :content, presence: true
 
-  belongs_to :tag
+  belongs_to :tag, optional: true
+  attr_accessor :tag_name
 end
