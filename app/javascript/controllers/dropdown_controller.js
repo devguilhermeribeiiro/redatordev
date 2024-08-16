f9febdc5216bd815ae21b0eb7063be5bd9ab3d53
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-15 22:06:16 -0300

    Improving the controller

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,15 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["menu"]
-  
+
   connect() {
-    this.element.addEventListener("mouseover", this.show.bind(this))
-    this.element.addEventListener("mouseleave", this.hide.bind(this))
+    this.showMenu = this.show.bind(this)
+    this.hideMenu = this.hide.bind(this)
+    
+    // Mostrar o menu quando o mouse entra no botão
+    this.element.addEventListener("mouseenter", this.showMenu)
+    
+    // Esconder o menu quando o mouse sai do menu
+    this.menuTarget.addEventListener("mouseleave", this.hideMenu)
   }
 
   disconnect() {
-    this.element.removeEventListener("mouseover", this.show.bind(this))
-    this.element.removeEventListener("mouseleave", this.hide.bind(this))
+    this.element.removeEventListener("mouseenter", this.showMenu)
+    this.menuTarget.removeEventListener("mouseleave", this.hideMenu)
   }
 
   show() {
